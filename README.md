34990c7667cb7a153bda5b09512ade30e9189d09
David <dleavenworth.ep@mysummitps.org>
2015-03-09 14:43:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# This is a small react calender application that I created for my CS class
---
+++ index.js
@@ -0,0 +1,14 @@
+var Header = React.createClass({
+  render: function() {
+    return 
+    <div class="navbar-fixed">
+       <nav>
+         <div class="nav-wrapper">
+            <a href="#" class="brand-logo center">Calander</a>
+            <ul id="nav-mobile" class="left hide-on-med-and-down">
+        </ul>
+    </div>
+  </nav>
+</div>
+  }
+});
