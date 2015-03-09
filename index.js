f4217dc382262f42b61b7a105fe3c33e6bd1c868
David <dleavenworth.ep@mysummitps.org>
2015-03-09 15:08:12 -0400

    Fixed rendering

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var Header = React.createClass({
   render: function() {
-    return 
-    <div class="navbar-fixed">
-       <nav>
+    return (
+      <div class="navbar-fixed">
+        <nav>
          <div class="nav-wrapper">
             <a href="#" class="brand-logo center">Calander</a>
             <ul id="nav-mobile" class="left hide-on-med-and-down">
-        </ul>
-    </div>
-  </nav>
-</div>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    );
   }
 });
+React.render(<Header/>, document.body);
