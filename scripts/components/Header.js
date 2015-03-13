e321687c5e96fa1b31c76623a68261ed9ec75663
David <dleavenworth.ep@mysummitps.org>
2015-03-13 12:30:06 -0400

    Fixed navbar

--- scripts/components/Header.js
+++ scripts/components/Header.js
@@ -10,9 +10,8 @@ export default React.createClass({
         <div className="navbar-fixed">
           <nav className="blue accent-4" style={{height: 90}}>
             <div className="nav-wrapper">
-              <a href="#" className="brand-logo center">Calendar</a>
+              <a href="http://everestps.github.io/third/" className="brand-logo center">Homepage</a>
               <ul id="nav-mobile" className="right-hide-on-med-and-down">
-                <li><a href="http://everestps.github.io/third/">Homepage</a></li>
               </ul>
             </div>
           </nav>
