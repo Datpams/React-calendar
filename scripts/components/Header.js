836de6a3115fb931246f61ea62594c1b534aca43
David <dleavenworth.ep@mysummitps.org>
2015-06-11 13:58:02 -0400

    Added navbar

--- scripts/components/Header.js
+++ scripts/components/Header.js
@@ -11,10 +11,14 @@ export default React.createClass({
           <nav className="blue accent-4" style={{height: 90}}>
             <div className="nav-wrapper">
               <a href="http://everestps.github.io/third/" className="brand-logo center">Homepage</a>
-              <ul id="nav-mobile" className="right-hide-on-med-and-down">
+              <ul className="right-hide-on-med-and-down">
                 <li><a href="http://everestps.github.io/third/tick.html">Tick tack toe</a></li>
                 <li><a href="http://everestps.github.io/third/calc.html">Calculator</a></li>
                 <li><a href="http://everestps.github.io/third/vind.html">Vindinium</a></li>
+                <li><a href="https://smtcs-chat.herokuapp.com/">React chat</a></li>
+                <li><a href="http://everestps.github.io/third/about.html">About me</a></li>
+                <li><a href="http://everestps.github.io/third/hardware.html">Hardware</a></li>
+                <li><a href="http://everestps.github/io/third/html.html">HTML</a></li>
               </ul>
             </div>
           </nav>
